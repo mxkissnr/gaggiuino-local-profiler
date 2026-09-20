36bc996f7840cf6721c1d6fe8d2914c4c772688d
Paul Lukas Schäfer <pl@pl-sc.de>
2026-09-20 10:36:36 +0200

    fix: escape machineId in custom-maint-task button attribute (CodeQL DOM-XSS)

--- gaggiuino-local-profiler/public-src/views/maintenance.ts
+++ gaggiuino-local-profiler/public-src/views/maintenance.ts
@@ -298,7 +298,7 @@ function _renderCustomSection(container: HTMLElement | null, scope: MaintScope):
           <label>Alle <input type="number" id="customTaskShots" min="1" max="10000" placeholder="–"> Bezüge</label>
           <label>Alle <input type="number" id="customTaskDays" min="1" max="3650" placeholder="–"> Tage</label>
         </div>
-        <button data-action="add-custom-maint-task" data-machine-id="${writeMid}">Hinzufügen</button>
+        <button data-action="add-custom-maint-task" data-machine-id="${esc(writeMid)}">Hinzufügen</button>
       </div>
     </details>
   `;
